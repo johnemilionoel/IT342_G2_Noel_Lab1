1677ca7cafa540e269252dc69c506e46e9b7ca54
LAB24_USER ON WS36 <L24X07W36@LabHCI.net>
2026-02-09 08:59:59 +0800

    Add UserRepository

---
+++ backend/g2app/src/main/java/com/noel/g2app/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.noel.g2app.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
